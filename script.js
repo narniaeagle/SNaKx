b9762630af5aa51b4b43809500d741957923374e
narniaeagle <ilgar2023@hotmail.com>
2023-02-04 17:54:36 -0500

    filled the board

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let FPS = 1000 / 15; // 15fps
 let loop;
+let boardColor = "rgb(6, 61, 21)";
+const board = document.querySelector("#board");
+let w = board.width;
+let h = board.height;
 
+
+
+const ctx = board.getContext("2d"); // drawing tool
 
 loop = setInterval(Frame,FPS) // 66ms * 15 = 1000ms (calling this function 15 times in a second)
 function Frame(){ 
@@ -9,8 +16,10 @@ function Frame(){
     Board();
 }
 
-function Board(){ // going to draw the board in here (filling canvas with a color)
+function Board(){
+    ctx.fillStyle = boardColor;
 
+    ctx.fillRect(0, 0, w, h);
 }
 
 function Snake(){ // draw the snake
