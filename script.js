739196e994ca0ccfc9c9a471e868c84d8fa471ec
narniaeagle <ilgar2023@hotmail.com>
2023-02-04 18:56:53 -0500

    measurements are now made by squares

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ let w = board.width;
 let h = board.height;
 
 
+let sqrSize = 20;
+let x = w / sqrSize; // number of squares on the x line
+let y = h / sqrSize; // number of the squares on the y line
+
+
 
 const ctx = board.getContext("2d"); // drawing tool
 
@@ -14,12 +19,21 @@ function Frame(){
     Food();
     Snake();
     Board();
+    Square(9,9,"red");
+    Square(9,8,"red");
 }
 
 function Board(){
     ctx.fillStyle = boardColor;
 
     ctx.fillRect(0, 0, w, h);
+}
+function Square(x, y, color){
+    ctx.fillStyle = color;
+
+    ctx.fillRect(x * sqrSize, y * sqrSize, sqrSize, sqrSize);   // filling the block
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(x * sqrSize, y * sqrSize, sqrSize, sqrSize); // outline at the same position
 }
 
 function Snake(){ // draw the snake
